efe3d92df7ec5ab7e50c6451f7ef291e80b5a09e
Tabares Sotelo, Jose <jose.tabares.sotelo@accenture.com>
2016-08-29 18:07:33 -0500

    Added data sample and package.js

---
+++ scr/data/frameworks.js
@@ -0,0 +1,31 @@
+
+const athletes = [
+  {
+    'id': 'react',
+    'name': 'React',
+    'country': 'cu',
+    'birth': '1973',
+    'image': 'driulis-gonzalez.jpg',
+    'cover': 'driulis-gonzalez-cover.jpg',
+    'link': 'https://en.wikipedia.org/wiki/Driulis_González',
+    'medals': [
+      { 'year': '1992', 'type': 'B', 'city': 'Barcelona', 'event': 'Olympic Games', 'category': '-57kg' },
+      { 'year': '1993', 'type': 'B', 'city': 'Hamilton', 'event': 'World Championships', 'category': '-57kg' },
+
+    ],
+  },
+  {
+    'id': 'angular',
+    'name': 'AngularJS',
+    'country': 'cu',
+    'birth': '1973',
+    'image': 'driulis-gonzalez.jpg',
+    'cover': 'driulis-gonzalez-cover.jpg',
+    'link': 'https://en.wikipedia.org/wiki/Driulis_González',
+    'medals': [
+      { 'year': '1992', 'type': 'B', 'city': 'Barcelona', 'event': 'Olympic Games', 'category': '-57kg' },
+      { 'year': '1993', 'type': 'B', 'city': 'Hamilton', 'event': 'World Championships', 'category': '-57kg' },
+
+    ],
+  }
+];
